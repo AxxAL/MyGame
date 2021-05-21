870d6bcaf4070c53e15c613de7ed15a3a398f587
Axel <ageinthehouse@gmail.com>
2021-05-21 16:12:38 +0200

    Glömde lite kod

--- MyGame/GameRoot.cs
+++ MyGame/GameRoot.cs
@@ -9,15 +9,15 @@ namespace MyGame
     public class GameRoot : Game
     {
         private Song music;
+        private HUD hud;
+        private Menu menu;
+        private HealthPackManager HealthPackManager;
         public GraphicsDeviceManager graphics;
         public ContentManager content;
         public SpriteBatch spriteBatch;
         public EnemyManager EnemyManager;
-        public HealthPackManager HealthPackManager;
         public Player player;
-        public HUD hud;
-        
-        
+
         public GameRoot()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -35,6 +35,7 @@ namespace MyGame
             this.EnemyManager = new EnemyManager(this);
             this.hud = new HUD(this);
             this.HealthPackManager = new HealthPackManager(this);
+            this.menu = new Menu(this);
             this.Music();
             base.Initialize();
         }
@@ -48,24 +49,44 @@ namespace MyGame
         {
             KeyboardState kState = Keyboard.GetState();
 
-            this.player.Update(gameTime, kState);
-            this.EnemyManager.Update(gameTime);
-            this.hud.Update(gameTime);
-            this.HealthPackManager.Update(gameTime);
-
+            if (this.menu.isMenuActive)
+            {
+                this.menu.Update(gameTime);
+            }
+            
+            if (!this.menu.isMenuActive)
+            {
+                if (kState.IsKeyDown(Keys.Escape))
+                {
+                    this.menu.isMenuActive = true;
+                }
+                this.player.Update(gameTime, kState);
+                this.EnemyManager.Update(gameTime);
+                this.hud.Update(gameTime);
+                this.HealthPackManager.Update(gameTime);
+            }
+            
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(new Color(69, 34, 86));
             this.spriteBatch.Begin();
 
-            this.player.Draw();
-            this.EnemyManager.Draw();
-            this.hud.Draw();
-            this.HealthPackManager.Draw();
+            if (this.menu.isMenuActive)
+            {
+                this.menu.Draw();
+            }
 
+            if (!this.menu.isMenuActive)
+            {
+                GraphicsDevice.Clear(new Color(69, 34, 86));
+                this.player.Draw();
+                this.EnemyManager.Draw();
+                this.hud.Draw();
+                this.HealthPackManager.Draw();
+            }
+            
             this.spriteBatch.End();
             base.Draw(gameTime);
         }
--- MyGame/Menu.cs
+++ MyGame/Menu.cs
@@ -1,7 +1,67 @@
-﻿namespace MyGame
+﻿using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame
 {
     public class Menu
     {
-        
+        private GameRoot game;
+        private Texture2D button;
+        private SpriteFont font;
+        private MouseState mouseState;
+        private Rectangle mouseHitbox;
+        private Vector2[] buttonPos;
+        private Rectangle[] buttonHitbox;
+        public bool isMenuActive;
+
+        public Menu(GameRoot game)
+        {
+            this.game = game;
+            this.isMenuActive = true;
+            this.button = this.game.content.Load<Texture2D>("sprites/button");
+            this.font = this.game.content.Load<SpriteFont>("sprites/font");
+            
+            this.buttonPos = new Vector2[]
+            {
+                new Vector2(100, 100),
+                new Vector2(100, 220)
+            };
+            this.buttonHitbox = new Rectangle[]
+            {
+                new Rectangle((int) this.buttonPos[0].X,(int) this.buttonPos[0].Y, this.button.Width, this.button.Height),
+                new Rectangle((int) this.buttonPos[1].X,(int) this.buttonPos[1].Y, this.button.Width, this.button.Height)
+            };
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            this.mouseState = Mouse.GetState();
+            this.mouseHitbox = new Rectangle(this.mouseHitbox.X, this.mouseState.Y, 10, 10);
+
+            if (this.mouseState.X > this.buttonPos[0].X && this.mouseState.X < this.buttonPos[0].X + this.button.Width && this.mouseState.Y > this.buttonPos[0].Y 
+                && this.mouseState.Y < this.buttonPos[0].Y + this.button.Height && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                this.isMenuActive = false;
+                Debug.WriteLine("PLAYING GAME");
+            }
+
+            if (this.mouseState.X > this.buttonPos[1].X && this.mouseState.X < this.buttonPos[1].X + this.button.Width && this.mouseState.Y > this.buttonPos[1].Y 
+                && this.mouseState.Y < this.buttonPos[1].Y + this.button.Height && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                game.Exit();
+            }
+        }
+
+        public void Draw()
+        {
+            this.game.GraphicsDevice.Clear(new Color(0, 86, 86));
+            this.game.spriteBatch.Draw(this.button, this.buttonHitbox[0], Color.White);
+            this.game.spriteBatch.Draw(this.button, this.buttonHitbox[1], Color.White);
+            
+            this.game.spriteBatch.DrawString(this.font, "Play", new Vector2(this.buttonPos[0].X + 80, this.buttonPos[0].Y + 40), Color.Black);
+            this.game.spriteBatch.DrawString(this.font, "Exit", new Vector2(this.buttonPos[1].X + 80, this.buttonPos[1].Y + 40), Color.Black);
+        }
     }
 }
